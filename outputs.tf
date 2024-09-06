ec27e1bdd428db4b0441c2b2d1276d712278b55f
Rich Green <richard.green@digital.justice.gov.uk>
2024-09-06 12:03:45 +0100

    update output attribute

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "aws_chatbot_slack_channel_arn" {
-  value = aws_chatbot_slack_channel_configuration.this.arn
+  value = aws_chatbot_slack_channel_configuration.this.chat_configuration_arn
 }
 output "aws_chatbot_iam_role" {
   value = aws_iam_role.this.arn
